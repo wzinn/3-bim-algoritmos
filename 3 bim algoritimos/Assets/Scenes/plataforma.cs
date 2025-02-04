c399b21f5a23aca7ca1c768315e45a13e3687e54
wzinn <ws91443973@gmail.com>
2025-02-04 16:05:54 -0300

    plataforma cair

---
+++ 3 bim algoritimos/Assets/Scenes/plataforma.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataforma : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float falldelay = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, falldelay);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      
+    }
+
+    // Update is called once per frame
+}
